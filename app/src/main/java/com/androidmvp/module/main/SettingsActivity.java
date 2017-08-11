26116ceec9476888478d79a5670e9bf99bb1537e
WillYang <willyang92@foxmail.com>
2017-08-11 17:32:17 +0800

    添加SettingsActivity

---
+++ app/src/main/java/com/androidmvp/module/main/SettingsActivity.java
@@ -0,0 +1,10 @@
+package com.androidmvp.module.main;
+
+import com.androidmvp.module.base.BaseSwipeBackActivity;
+
+/**
+ * Created by admin on 2017/8/11.
+ */
+
+public class SettingsActivity extends BaseSwipeBackActivity {
+}
